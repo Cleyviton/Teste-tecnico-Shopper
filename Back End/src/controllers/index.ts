a8a306e2d44958bfe43460ce54a509a0d71e01ca
Cleyviton <114519180+Cleyviton@users.noreply.github.com>
2023-09-06 16:33:22 -0300

    Update index.ts

--- Back End/src/controllers/index.ts
+++ Back End/src/controllers/index.ts
@@ -2,14 +2,12 @@ import {
     validateCsv,
     updateProducts,
     listProducts,
-    listPacks,
 } from "./products.controllers";
 
 const controllers = {
     validateCsv,
     updateProducts,
     listProducts,
-    listPacks,
 };
 
 export default controllers;
